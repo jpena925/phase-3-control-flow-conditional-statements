94b564305762a94cc1cebc342b67a35baaad0d1f
Jack Pena <penajackc@gmail.com>
2022-02-19 12:08:19 -0700

    commit tree 3ea4d0b709fae6d6a2a06fa5f62b30817601c9d3

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,41 @@
 def admin_login(username, password)
-  # your code here
+  return "Access granted" if (username == 'ADMIN' || username == 'admin') && password == "12345"
+  return "Access denied"
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  case temperature
+  when 0...40 
+    "It's brisk out there!"
+  when 40...65 
+    "It's a little chilly out there!"
+  when 85...120 
+    "It's too dang hot out there!"
+  else 
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 15 == 0
+    "FizzBuzz"
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else 
+    num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when "+" then num1 + num2
+  when "-" then num1 - num2
+  when "*" then num1 * num2
+  when "/" then num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
